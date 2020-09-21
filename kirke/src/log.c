3d15e903a0c10e12702fe9db8480787787574c09
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-21 13:04:06 -0500

    Added implementation for log__level__to_string.

--- kirke/src/log.c
+++ kirke/src/log.c
@@ -2,6 +2,18 @@
 #include "kirke/log.h"
 
 char* log__level__to_string( Log__Level log_level ){
-    (void)( log_level );
-    return "";
+    switch( log_level ){
+        case Log__Level__Verbose:
+            return "Verbose";
+        case Log__Level__Debug:
+            return "Debug";
+        case Log__Level__Info:
+            return "Info";
+        case Log__Level__Warning:
+            return "Warning";
+        case Log__Level__Error:
+            return "Error";
+        default:
+            return "INVALID";
+    }
 }
