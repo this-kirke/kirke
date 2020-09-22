ba190ec4240b2cae12658968c5324e27b3654252
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 17:35:58 -0500

    Added method io__read_text_file.  This method reads the contents of a text file into a newly-allocated String, which is returned to the caller.
    Added test io__read_text_file.

---
+++ kirke/src/io.c
@@ -0,0 +1,13 @@
+// Internal Includes
+#include "kirke/allocator.h"
+#include "kirke/error.h"
+#include "kirke/io.h"
+#include "kirke/string.h"
+
+String* io__read_text_file( Allocator* allocator, String* file_path, Error* error ){
+    (void)( allocator );
+    (void)( file_path );
+    (void)( error );
+
+    return NULL;
+}
---
+++ kirke/test/test__libkirke__io.cpp
@@ -0,0 +1,46 @@
+// 3rdParty Includes
+#include "catch2/catch.hpp"
+
+// Internal Includes
+#include "kirke/error.h"
+#include "kirke/io.h"
+#include "kirke/system_allocator.h"
+#include "kirke/string.h"
+
+class IOTestFixture {
+    protected:
+        IOTestFixture(){
+            system_allocator__initialize( &system_allocator, NULL );
+
+            // Create an input file of known size
+            FILE* output_file = fopen( file_path.data, "w" );
+            unsigned long long bytes_written = fwrite( file_contents.data, 1, file_contents.length, output_file );
+            REQUIRE( bytes_written == file_contents.length );
+            fclose( output_file );
+        }
+
+        ~IOTestFixture(){
+            system_allocator__deinitialize( &system_allocator );
+
+            // Remove our input file
+            remove( file_path.data );
+        }
+
+        SystemAllocator system_allocator;
+
+        String file_path = string__literal( "test_file.txt" );
+        String file_contents = string__literal( "Test File Contents:  012345\n" );
+};
+
+TEST_CASE_METHOD( IOTestFixture, "io__read_text_file", "[io]" ){
+    Error error = {0};
+    String* input = io__read_text_file( system_allocator.allocator, &file_path, &error );
+
+    REQUIRE( error.code == Error__None );
+    REQUIRE( input != NULL );
+    REQUIRE( input->length == file_contents.length );
+    REQUIRE( string__equals( input, &file_contents ) );
+
+    string__clear( input, system_allocator.allocator );
+    allocator__free( system_allocator.allocator, input );
+}
