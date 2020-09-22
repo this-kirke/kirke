4f20849b69ee0f4b5406c7e4e74ed70701133a41
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 14:22:44 -0500

    Added convenience macro string__literal. This macro initializes a String structure from a string literal expression.
    Added test string__literal__array.
    
    Added test string__literal__inline.

--- kirke/include/kirke/string.h
+++ kirke/include/kirke/string.h
@@ -20,6 +20,18 @@ ARRAY__DECLARE( String, string, char )
 ARRAY__DECLARE( Array__String, array__string, String )
 
 /**
+ *  \def string__literal( TEXT )
+ *  \brief This macro defines a String object containing the specified text.
+ *  \param TEXT A pointer to the an array of characters which will be contained by the defined String.
+ */
+#define string__literal( TEXT ){        \
+        .data = (char*) TEXT,           \
+        .length = sizeof( TEXT ) - 1,   \
+        .capacity = sizeof( TEXT ),     \
+        .element_size = sizeof( char )  \
+    }
+
+/**
  *  @} group log
  */
 
--- kirke/test/test__libkirke__string.cpp
+++ kirke/test/test__libkirke__string.cpp
@@ -4,3 +4,24 @@
 // Internal Includes
 #include "kirke/string.h"
 #include "kirke/system_allocator.h"
+
+TEST_CASE( "string__literal__array", "[string]" ){
+    char string_literal[] = "This is a string.";
+
+    String string = string__literal( string_literal );
+
+    REQUIRE( string.data == string_literal );
+    REQUIRE( string.length == sizeof( string_literal ) - 1 );
+    REQUIRE( string.capacity == sizeof( string_literal ) );
+    REQUIRE( string.element_size == sizeof( char ) );
+}
+
+TEST_CASE( "string__literal__inline", "[string]" ){
+    const char string_literal[] = "This is a string.";
+    String string = string__literal( "This is a string." );
+
+    REQUIRE( strcmp( string.data, string_literal ) == 0 );
+    REQUIRE( string.length == sizeof( string_literal ) - 1 );
+    REQUIRE( string.capacity == sizeof( string_literal ) );
+    REQUIRE( string.element_size == sizeof( char ) );
+}
