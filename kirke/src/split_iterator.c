68b6c15acaeca766bb74e4d920b4cb253960bda6
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 17:16:04 -0500

    Added implementation for method split_iterator__initialize.

--- kirke/src/split_iterator.c
+++ kirke/src/split_iterator.c
@@ -2,8 +2,8 @@
 #include "kirke/split_iterator.h"
 #include "kirke/string.h"
 
-void split_iterator__initialize( SplitIterator* iterator, const String* string, const String* delimiter ){
-    (void)( iterator );
-    (void)( string );
-    (void)( delimiter );
+void split_iterator__initialize( SplitIterator* iterator, const String* string_, const String* delimiter ){
+    iterator->string = string_;
+    iterator->delimiter = delimiter;
+    iterator->position = 0;
 }
