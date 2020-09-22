f9441b2d1c30f50be67624b1db87ed6253824b9d
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 17:36:20 -0500

    Added implementation for io__read_text_file.

--- kirke/src/io.c
+++ kirke/src/io.c
@@ -5,9 +5,30 @@
 #include "kirke/string.h"
 
 String* io__read_text_file( Allocator* allocator, String* file_path, Error* error ){
-    (void)( allocator );
-    (void)( file_path );
-    (void)( error );
+    FILE* input_file = fopen( file_path->data, "r" );
 
-    return NULL;
+    if( input_file == NULL ){
+        error__set(
+            error,
+            "IO",
+            IO__Error__UnableToOpenFile,
+            "Unable to open input file \"%s.\"", file_path->data
+        );
+
+        return NULL;
+    }
+
+    unsigned long long input_file_size;
+    fseek( input_file, 0, SEEK_END );
+    input_file_size = ftell( input_file );
+
+    String* output = allocator__alloc( allocator, sizeof( String ) );
+    string__initialize( output, allocator, input_file_size );
+
+    fseek( input_file, 0, SEEK_SET );
+    output->length = fread( output->data, 1, input_file_size, input_file );
+
+    fclose( input_file );
+
+    return output;
 }
