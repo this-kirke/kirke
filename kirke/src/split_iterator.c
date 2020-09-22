96891b68bcf7fcdd0c90fdc306cacae5faa8efd9
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 17:20:10 -0500

    Added implementation for method split_iterator__rest.

--- kirke/src/split_iterator.c
+++ kirke/src/split_iterator.c
@@ -9,6 +9,10 @@ void split_iterator__initialize( SplitIterator* iterator, const String* string_,
 }
 
 void split_iterator__rest( SplitIterator* iterator, String* ref_rest ){
-    (void)( iterator );
-    (void)( ref_rest );
+    *ref_rest = (String){
+        .data = iterator->string->data + iterator->position,
+        .length = iterator->string->length - iterator->position,
+        .capacity = iterator->string->length - iterator->position,
+        .element_size = sizeof( char )
+    };
 }
