d98f777c6856358a3927ae4e175c4921107089c5
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 15:40:40 -0500

    Added implementation for string__initialize__va_list.

--- kirke/src/string.c
+++ kirke/src/string.c
@@ -16,8 +16,11 @@ ARRAY__DEFINE( String, string, char, chars_are_equal )
 ARRAY__DEFINE( Array__String, array__string, String, string__equals )
 
 void string__initialize__va_list( String* string, Allocator* allocator, const char* format, va_list args ){
-    (void)( string );
-    (void)( allocator );
-    (void)( format );
-    (void)( args );
+    char c;
+    va_list args_copy;
+    va_copy( args_copy, args );
+    int required_capacity = vsnprintf( &c, 1, format, args_copy ) + 1;
+
+    string__initialize( string, allocator, required_capacity );
+    string->length = vsnprintf( string->data, required_capacity, format, args );
 }
