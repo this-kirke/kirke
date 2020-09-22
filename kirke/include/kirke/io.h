48b6fa9428d9740f22272194842a96f5ab9f45a0
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-22 17:35:58 -0500

    Added method io__read_text_file.  This method reads the contents of a text file into a newly-allocated String, which is returned to the caller.
    Added test io__read_text_file.

---
+++ kirke/include/kirke/io.h
@@ -0,0 +1,51 @@
+/**
+ *  \file kirke/io.h
+ */
+
+#ifndef KIRKE__IO__H
+#define KIRKE__IO__H
+
+// Internal Includes
+#include "kirke/macros.h"
+
+BEGIN_DECLARATIONS
+
+/** Forward declaration of Allocator, defined in \ref kirke/allocator.h */
+typedef struct Allocator Allocator;
+
+/** Forward declaration of Error, defined in \ref kirke/error.h */
+typedef struct Error Error;
+
+/** Forward declaration of String, defined in \ref kirke/string.h */
+typedef struct String String;
+
+/**
+ *  \defgroup io IO
+ *  @{
+ */
+
+/**
+ *  \brief This enumerator defines possible Error types for IO functions.
+ */
+typedef enum IO__Error {
+    /** \brief Denotes that an error occurred because the specified file could not be found. */
+    IO__Error__UnableToOpenFile = 1
+} IO__Error;
+
+/**
+ *  \brief This method reads the contents of the file located at \p file_path into a newly-allocated Slice.
+ *  \param allocator A pointer to the Allocator which will be used to allocate memory controlled by the returned Slice.
+ *  \param file_path A pointer to a String containing the path of the file to be read.
+ *  \param error Optional. The address of a pointer to an Error structure, which will be non-NULL upon return if an error is
+ *  encountered while attempting to read the specified file.
+ *  \returns A pointer to a newly-allocated String containing the contents of the file.
+ */
+String* io__read_text_file( Allocator* allocator, String* file_path, Error* error );
+
+/**
+ *  @} group io
+ */
+
+END_DECLARATIONS
+
+#endif // KIRKE__IO__H
