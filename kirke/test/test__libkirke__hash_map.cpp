bb9e5f2830b957b0f349cc92f2f8a8071592b78d
Justin Kirkegaard <justin.kirkegaard@gmail.com>
2020-09-27 23:44:12 -0500

    Added method hash_map__delete. This method deletes the entry corresponding to the specified key from a hash map.
    Added test hash_map__delete.

--- kirke/test/test__libkirke__hash_map.cpp
+++ kirke/test/test__libkirke__hash_map.cpp
@@ -56,3 +56,29 @@ TEST_CASE_METHOD( HashMap__TestFixture, "hash_map__insert_and_retrieve", "[hash_
     String does_not_exit = string__literal( "Does not exist." );
     REQUIRE_FALSE( hash_map__string_to_int__retrieve( &hash_map, does_not_exit, &retrieved_value ) );
 }
+
+TEST_CASE_METHOD( HashMap__TestFixture, "hash_map__delete", "[hash_map]" ){
+    String strings[ 10 ] = {
+        string__literal( "zero" ),
+        string__literal( "one" ),
+        string__literal( "two" ),
+        string__literal( "three" ),
+        string__literal( "four" ),
+        string__literal( "five" ),
+        string__literal( "six" ),
+        string__literal( "seven" ),
+        string__literal( "eight" ),
+        string__literal( "nine" )
+    };
+
+    for( unsigned long entry_index = 0; entry_index < 10; entry_index++ ){
+        hash_map__string_to_int__insert( &hash_map, strings[ entry_index ], entry_index );
+    }
+
+    for( unsigned long entry_index = 0; entry_index < 10; entry_index++ ){
+        hash_map__string_to_int__delete( &hash_map, strings[ 9 - entry_index ] );
+
+        int value;
+        REQUIRE_FALSE( hash_map__string_to_int__retrieve( &hash_map, strings[ 9 - entry_index ], &value ) );
+    }
+}
